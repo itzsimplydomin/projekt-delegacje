947fc976cba85845021d30ae1fb26a60881cadd3
Dominik <domi.pakula@gmail.com>
2025-10-29 18:13:06 +0100

    push

---
+++ projekt-delegacje/src/constants/colors.ts
@@ -0,0 +1,10 @@
+export const colors = {
+  navyDarkest: '#0A1A3A',
+  navyDarker: '#142857',
+  navy: '#002E7A',
+  oceanBlue: '#6A9BD8',
+  paleBlue: '#C8D8EB',
+  lightBg: '#F5F9FF',
+  text: '#2D3748',
+  white: '#FFFFFF',
+} as const;
