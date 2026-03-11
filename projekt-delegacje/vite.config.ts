6f43a1054839c2d824126825d72fa747f93e18e3
itzsimplydomin <domi.pakula@gmail.com>
2026-03-11 12:58:08 +0100

    PWA

--- projekt-delegacje/vite.config.ts
+++ projekt-delegacje/vite.config.ts
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Delegacje Artikon",
+        short_name: "Delegacje",
+        description: "System zarządzania delegacjami",
+        theme_color: "#002E7A",
+        background_color: "#002E7A",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "./img/web-app-manifest-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "./img/web-app-manifest-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
